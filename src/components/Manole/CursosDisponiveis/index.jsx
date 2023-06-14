8478a3f0caa61ceb2f8309d54622f5e14e0de834
pauloandremarx <pauloandremarx@hotmail.com>
2023-06-14 17:58:08 -0300

    pagination inffinty

--- src/components/Manole/CursosDisponiveis/index.jsx
+++ src/components/Manole/CursosDisponiveis/index.jsx
@@ -1,71 +1,142 @@
 "use client";
 
-import React, { useState } from "react";
-import styles from "./CursosDisponiveis.module.css";
-import { useQueries } from "@tanstack/react-query";
-import { getRecomendacoes } from "@/services/recomendacoes/userRecomendacoes";
-import usePostLogs from "@/services/Postlogs/usePostLogs";
-import useCadastroLogado from "@/services/cadastrarLogado/useCadastroLogado";
-import { getLogs } from "@/services/GetLogs/useGetLogs";
-import { useSession } from "next-auth/react";
+import React, {useState} from "react";
+import {useInfiniteQuery} from "@tanstack/react-query";
+import {getRecomendacoes} from "@/services/recomendacoes/userRecomendacoes";
+import {useSession} from "next-auth/react";
+import Config from "@/util/Config";
+import styles from './CursosDisponiveis.module.css';
 
 export default function ContainerCursosDisponiveis(props) {
-    const [page, setPage] = React.useState(0);
 
-    const { data: session, status } = useSession();
 
+    const { data: session, status_a } = useSession();
     const usu_id = session?.user?.decode?.usu_id;
 
-    const [todos, setTodos] = useState([]);
 
-    const [recomendacoes] = useQueries({
-        queries: [
+    const fetchRecomendacoes = async ({ pageParam  = 0, user_id = usu_id }) => {
+        let from = 0 ;
+        let to = 19;
+        const res = await fetch(
+            `https://nwlax96g00.execute-api.us-east-1.amazonaws.com/recomendacao/products?from=${from + pageParam}&to=${from + to}&user=${user_id}`,
             {
-                queryKey: ["recomendacoes", page],
-                queryFn: () => getRecomendacoes(page, usu_id),
-                enabled: !!usu_id,
-                keepPreviousData: true,
-            },
-        ],
-    });
+                method: "GET",
+                headers: {
+                    'Authorization': Config.API_KEY,
+                },
+            }
+        );
+
+        if (!res.ok) {
+            throw new Error(res.statusText);
+        }
+
+        return await res.json();
+    }
 
 
- 
+    let pageParam = 0;
+    const {
+        data,
+        error,
+        fetchNextPage,
+        hasNextPage,
+        isFetching,
+        isFetchingNextPage,
+        status,
+    } = useInfiniteQuery({
+        queryKey: ['projects', pageParam],
+        queryFn: ({pageParam = 0}) => fetchRecomendacoes({  pageParam }),
 
 
-    if (recomendacoes.isLoading) return "Caregando cursos...";
-    if (recomendacoes.error) return recomendacoes.error.message;
+        enabled: !!usu_id,
+        getNextPageParam: (lastPage, allPages) => {
+            const nextPage =
+                lastPage.length > 0 && lastPage.length < 100 ? allPages.length - 1 : undefined;
+            return nextPage;
+        },
+        getPreviousPageParam: (firstPage, pages) => firstPage.prevCursor,
+    })
 
-    return (
+
+
+    return status === 'loading' ? (
+        <p>Loading...</p>
+    ) : status === 'error' ? (
+        <p>Error: {error.message}</p>
+    ) : (
         <>
-            <span>Current Page: {page}</span>
-
-            <button
-                onClick={() => setPage(page + 10)}
-                disabled={recomendacoes.isPreviousData}
-            >
-                Carregar mais
-            </button>
             <div className={`uk-child-width-1-5@m`} data-uk-grid="">
-                {todos.data?.map(function (item, i) {
-                    return (
-                        <div key={item.name + i}>
-                            <div className={`${styles.card}`}>
-                                <h1 className={`${styles.titulo}`}>{item.name}</h1>
+                {data.pages.map((group, i) => (
+                    <React.Fragment key={i}>
+                        {group.map((recomendation_item) => (
+                            <div key={recomendation_item.name + i}>
+                                <div className={`${styles.card}`}>
+                                    <h1 className={`${styles.titulo}`}>{recomendation_item.name}</h1>
 
-                                <div
-                                    className={`${styles.thumbmail}`}
-                                    style={{
-                                        backgroundImage: `url( ${item?.items[0]?.images[0]?.imageUrl})`,
-                                    }}
-                                ></div>
+                                    <div
+                                        className={`${styles.thumbmail}`}
+                                        style={{
+                                            backgroundImage: `url( ${recomendation_item?.items[0]?.images[0]?.imageUrl})`,
+                                        }}
+                                    ></div>
 
-                                <a className={`${styles.saber_mais}`}>Saber Mais</a>
+                                    <a className={`${styles.saber_mais}`}>Saber Mais</a>
+                                </div>
                             </div>
-                        </div>
-                    );
-                })}
+                        ))}
+                    </React.Fragment>
+                ))}
             </div>
+            <div>
+                <button
+                    onClick={() => fetchNextPage()}
+
+                >
+                    {isFetchingNextPage
+                        ? 'Loading more...'
+                        : hasNextPage
+                            ? 'Load More'
+                            : 'Nothing more to load'}
+                </button>
+            </div>
+            <div>{isFetching && !isFetchingNextPage ? 'Fetching...' : null}</div>
         </>
-    );
+    )
+
+
+ /*
+    return (
+
+     <>
+         <span>Current Page: {page}</span>
+
+         <button
+             onClick={() => setPage(page + 10)}
+             disabled={recomendacoes.isPreviousData}
+         >
+             Carregar mais
+         </button>
+         <div className={`uk-child-width-1-5@m`} data-uk-grid="">
+             {todos.data?.map(function (item, i) {
+                 return (
+                     <div key={item.name + i}>
+                         <div className={`${styles.card}`}>
+                             <h1 className={`${styles.titulo}`}>{item.name}</h1>
+
+                             <div
+                                 className={`${styles.thumbmail}`}
+                                 style={{
+                                     backgroundImage: `url( ${item?.items[0]?.images[0]?.imageUrl})`,
+                                 }}
+                             ></div>
+
+                             <a className={`${styles.saber_mais}`}>Saber Mais</a>
+                         </div>
+                     </div>
+                 );
+             })}
+         </div>
+     </>
+ )*/
 }
